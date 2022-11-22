0d64e053144ef9ce6d909f18281001a7903b9253
ck07022 <jeannenkim0202@gmail.com>
2022-11-22 19:45:46 +0900

    feat: 모델 만들기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 # app = Flask(__name__)
 
 # @app.route('/')
@@ -7,7 +15,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # 블루프린트
     from .views import main_views
     app.register_blueprint(main_views.bp)
 
