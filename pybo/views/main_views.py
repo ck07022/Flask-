3b6dc7f709c05879672c1e5c1a99e463c8b6579b
ck07022 <jeannenkim0202@gmail.com>
2022-11-22 20:50:18 +0900

    feat: 질문 목록 화면에 표시허기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -5,9 +5,10 @@ from pybo.models import Question
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
 
 @bp.route('/')
 def index():
