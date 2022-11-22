a379dd3d720c7bc91f0c0e663f9c7e0091fbebca
ck07022 <jeannenkim0202@gmail.com>
2022-11-22 20:56:05 +0900

    feat: 404 오류 페이지 표시하기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -7,7 +7,7 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
-    question = Question.query.get(question_id)
+    question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question)
 
 @bp.route('/')
