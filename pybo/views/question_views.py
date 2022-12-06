1872ba3454737da97235d6191f903d8a0d4ae7eb
ck07022 <jeannenkim0202@gmail.com>
2022-12-06 20:32:55 +0900

    feat: 폼 데이터를 저장하는 코드 작성하기

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from datetime import datetime
 
+from flask import Blueprint, render_template, request, url_for
+from werkzeug.utils import redirect
+
+from pybo import db
 from pybo.models import Question
-
 from pybo.forms import QuestionForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -19,4 +22,9 @@ def detail(question_id):
 @bp.route('/create/', methods=('GET','POST'))
 def create():
     form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
